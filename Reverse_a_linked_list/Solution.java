e05f56ebf7d072bec9512fbcc4bc6f20510fcb89
haiyoushei1 <1260404943@qq.com>
2021-08-17 22:53:13 +0800

    feat：反转链表

---
+++ Reverse_a_linked_list/Solution.java
@@ -0,0 +1,21 @@
+package Reverse_a_linked_list;
+
+public class Solution {
+    public int[] reversePrint(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode nextNode = head.next;
+        ListNode tempNode = null;
+        while(nextNode != null){
+
+        }
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
